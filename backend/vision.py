ca0eb29a26af626dc78f87c9a6a3095ff89f04a2
Gokul <gokulprakash7341@gmail.com>
2026-07-26 13:31:12 +0530

    Update vision.py

--- backend/vision.py
+++ backend/vision.py
@@ -70,7 +70,7 @@ def send_posture_async(name, state):
     try:
         payload = {"name": name, "state": state}
         
-        requests.post("http://127.0.0.1:8000/update_posture", json=payload, timeout=5.0)
+        requests.post("https://awake-enterprise.onrender.com/update_posture", json=payload, timeout=5.0)
     except Exception as e:
         print(f"Connection Error: {e}")
 
